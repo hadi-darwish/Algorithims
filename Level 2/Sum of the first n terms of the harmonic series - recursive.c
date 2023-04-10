92db546bb3809a31c5db91ab3c7d2e6b42241d08
hadi-darwish <hadi.darwish.03@gmail.com>
2023-04-10 18:30:31 +0300

    Level 2 => Initial commit

---
+++ Level 2/Sum of the first n terms of the harmonic series - recursive.c
@@ -0,0 +1,12 @@
+// Write a recursive function float harm_rec(int n)
+// which computes the sum of the first n terms of the harmonic series (n is passed in parameter):
+// 1+1/2+1/3+⋯+1n
+
+#include <stdio.h>
+#include <conio.h>
+
+float harm_rec(int n)
+{
+
+    return
+}
